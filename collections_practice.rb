a454ba3a2b818a187fc531c8e8ef2c6576a4dd12
Raquel Randall <raquel.sae.randall@gmail.com>
2018-10-23 18:13:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -57,7 +57,7 @@ def contain_a(array)
 end
 
 def first_wa(array)
-  array.find { |elem| elem.start_with?('wa') }
+  array.find { |elem| elem.to_s.start_with?('wa') }
 end
 
 def remove_non_strings(array)
