224f501168c1772b3d83b7f1e9fa7960451df994
Raquel Randall <raquel.sae.randall@gmail.com>
2018-10-23 18:10:56 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,7 +1,7 @@
 # def begins_with_r(tools)
 #   tools.all? {|word| word.start_with?("r")}
 # end
-# 
+#
 # def contain_a(array)
 #   newArray = []
 #   array.each do |word|
@@ -9,38 +9,38 @@
 #   end
 #   newArray
 # end
-# 
+#
 # def first_wa(array)
 #   array.find {|word| word.to_s.start_with?("wa")}
 # end
-# 
+#
 # def remove_non_strings(array)
 #   array.delete_if {|elem| !(elem.is_a? String)}
 # end
-# 
+#
 # def count_elements(array)
 #   array.uniq.each do |word|
-#     count = 0 
+#     count = 0
 #     array.each do |word2|
-#       if word2 == word 
-#         count += 1 
+#       if word2 == word
+#         count += 1
 #       end
 #     end
 #     word[:count] = count
 #   end
 # end
-# 
+#
 # def merge_data(keys, data)
 #   final = []
 #   keys.each {|i| data.first.map {|key,value| if i.values[0] == key then final << i.merge(value) end}}
 #   final
 # end
-# 
-# 
+#
+#
 # def find_cool(array)
 #   array.select {|i| i.any? {|key,value| value == "cool"}}
 # end
-# 
+#
 # def organize_schools(array)
 #   locations = {}
 #   array.collect {|key, value| locations[value[:location]] = []}
